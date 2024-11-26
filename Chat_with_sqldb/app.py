eea9e87ce919139044f47244c6ad2d9868b0e7c1
Md Ali Armaghan <aliarmaghan78@gmail.com>
2024-11-26 21:27:12 +0530

    chat with sql project

---
+++ Chat_with_sqldb/app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from pathlib import Path
+from langchain.agents import create_sql_agent
+from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from sqlalchemy import create_engine
+import sqlite3
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
+groq_api_key=os.getenv("GROQ_API_KEY")
+
+st.set_page_config(page_title="LangChain: Chat with SQL DB", page_icon="🦜")
+st.title("🦜 LangChain: Chat with SQL DB")
+
+LOCALDB="USE_LOCALDB"
+MYSQL="USE_MYSQL"
+
+radio_opt=["Use SQLLite 3 Database","Connect to you MySQL Database","Connect to sql server"]
+
+selected_opt=st.sidebar.radio(label="Choose the DB which you want to chat",options=radio_opt)
+
+if radio_opt.index(selected_opt)==1:
+    db_uri=MYSQL
+    if not db_uri:
+        st.info("Please enter the database information and uri")
+    mysql_host=st.sidebar.text_input("Provide MySQL Host")
+    mysql_user=st.sidebar.text_input("MYSQL User")
+    mysql_password=st.sidebar.text_input("MYSQL password",type="password")
+    mysql_db=st.sidebar.text_input("MySQL database")
+elif radio_opt.index(selected_opt)==2:
+    st.warning("Working on it")
+else:
+    db_uri=LOCALDB
+
+
+
