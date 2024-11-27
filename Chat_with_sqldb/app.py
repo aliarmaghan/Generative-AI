77b008441f4c5cb461b58a7fbc376a637586ee3e
Md Ali Armaghan <aliarmaghan78@gmail.com>
2024-11-27 22:24:34 +0530

    integrated mysql to chat

--- Chat_with_sqldb/app.py
+++ Chat_with_sqldb/app.py
@@ -20,6 +20,7 @@ st.title("🦜 LangChain: Chat with SQL DB")
 
 LOCALDB="USE_LOCALDB"
 MYSQL="USE_MYSQL"
+SQLSERVER="USE_SQLSERVER"
 
 radio_opt=["Use SQLLite 3 Database","Connect to you MySQL Database","Connect to sql server"]
 
@@ -35,8 +36,60 @@ if radio_opt.index(selected_opt)==1:
     mysql_db=st.sidebar.text_input("MySQL database")
 elif radio_opt.index(selected_opt)==2:
     st.warning("Working on it")
+    db_uri=SQLSERVER
 else:
     db_uri=LOCALDB
 
 
+## LLM Model
+llm=ChatGroq(groq_api_key=groq_api_key,model="llama3-8b-8192",streaming=True)
 
+@st.cache_resource(ttl="2h")
+def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri==LOCALDB:
+        dbfilepath=(Path(__file__).parent/"student.db").absolute()
+        print(dbfilepath)
+        creator = lambda: sqlite3.connect(f"file:{dbfilepath}?mode=ro", uri=True)
+        return SQLDatabase(create_engine("sqlite:///", creator=creator))
+    elif db_uri==MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please provide all MySQL connection details.")
+            st.stop()
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))   
+
+
+if db_uri==MYSQL:
+    db=configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+elif db_uri==SQLSERVER:
+    # db=configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+    st.info("Under Maintenance")
+else:
+    db=configure_db(db_uri)
+
+## toolkit
+toolkit=SQLDatabaseToolkit(db=db,llm=llm)
+
+agent=create_sql_agent(
+    llm=llm,
+    toolkit=toolkit,
+    verbose=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+user_query=st.chat_input(placeholder="Ask anything from the database")
+
+if user_query:
+    st.session_state.messages.append({"role": "user", "content": user_query})
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        streamlit_callback=StreamlitCallbackHandler(st.container())
+        response=agent.run(user_query,callbacks=[streamlit_callback])
+        st.session_state.messages.append({"role":"assistant","content":response})
+        st.write(response)
