1cea0ff744a7eb56537706d980093a85bd6f9efd
Md Ali Armaghan <aliarmaghan78@gmail.com>
2024-12-29 15:17:47 +0530

    Change model to gemma2-9b-it

--- summarize_text/app.py
+++ summarize_text/app.py
@@ -19,7 +19,7 @@ st.subheader('Summarize URL')
 import os
 from dotenv import load_dotenv
 load_dotenv()
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model="Gemma-7b-It")
+llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model="gemma2-9b-it")
 youtube_api_key=os.getenv("YOUTUBE_API_KEY")
 generic_url=st.text_input("URL",label_visibility="collapsed")
 
